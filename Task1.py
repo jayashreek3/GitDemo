64324cde015aebbcf0c9449da113ab86f4df0ee0
jayashreek3 <jayashreeks.333@gmail.com>
2025-12-26 20:02:20 +0530

    Initial Tasks Commit

---
+++ Task1.py
@@ -0,0 +1,14 @@
+"""
+Task1: Basic Mathematical Operations
+"""
+num1 = int(input("Enter the number1: "))
+num2 = int(input("Enter the number2: "))
+
+Add = (num1 + num2)
+print("Addition:", Add)
+Sub = (num1 - num2)
+print("Subtraction:", Sub)
+Multiply = (num1 * num2)
+print("Multiplication:", Multiply)
+Divide = (num1 / num2)
+print("Division:", Divide)
---
+++ Task2.py
@@ -0,0 +1,8 @@
+"""
+Task2: Creating Personalised Greeting
+"""
+UserName = input("Enter the User 1st Name: ")
+UserLastname = input("Enter the User Last Name: ")
+
+FullName = (UserName + " " + UserLastname + "!")
+print("Hello,", FullName, 'Welcome to the python program.')
